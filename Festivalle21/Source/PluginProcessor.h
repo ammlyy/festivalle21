1fc88a6f374268c86f0ad75f05540efa9b381569
Gabriele Stucchi <gabriele.stucchi@mail.polimi.it>
2021-06-16 12:24:57 +0200

    mad project fix

--- Festivalle21/Source/PluginProcessor.h
+++ Festivalle21/Source/PluginProcessor.h
@@ -77,7 +77,7 @@ private:
     std::vector<std::vector<float>> av;
     float rms;
     int currentAVindex;
-    fdeep::model model = fdeep::load_model("C:\\Users\\amere\\source\\Repos\\festivalle21\\Festivalle21\\Source\\models\\exported\\exported_model.json");
+    fdeep::model model = fdeep::load_model("C:\\Users\\gabri\\festivalle21\\Festivalle21\\Source\\models\\exported\\exported_model.json");
     juce::AudioBuffer<float> bufferToFill;
     int bufferToFillSampleIdx;
     juce::OSCSender sender;
