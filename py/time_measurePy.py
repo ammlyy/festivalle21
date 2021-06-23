af46283250b5226e4428cbdcc16ca8e5aefe326e
ammlyy <amerenamatteo@gmail.com>
2021-06-23 19:30:15 +0200

    script to measure time in python

--- py/time_measurePy.py
+++ py/time_measurePy.py
@@ -7,8 +7,12 @@ pred_model = load_model("./Festivalle21/Source/models/best_model.hdf5", compile=
 opt = tf.keras.optimizers.Adam(learning_rate=0.001)
 pred_model.compile(optimizer=opt, loss="mse", metrics=[ tf.keras.metrics.RootMeanSquaredError() ])
 
-print('HERE')
 X_test = np.load("./py/test_song/X.npy")
-start_time = time.perf_counter()
-pred_model.predict(X_test[0])
-print(time.perf_counter() - start_time)
+
+print(X_test.shape)
+
+with open('timing_measurePython.txt', 'w') as f:
+    for i in range(60):
+        start_time = time.perf_counter()
+        pred_model.predict(X_test[i])
+        f.write((str(time.perf_counter() - start_time) + '\n'))
